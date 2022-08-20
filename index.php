6c88ca0d6371d708429ae16280fa1cb1ba2ed4a2
kauan101moreira <kauan.r.b.moreira@gmail.com>
2022-08-19 21:45:45 -0300

    Aula 19-08-22

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+    // Comentarios em PHP - Linha
+    /*
+
+    */
+    //echo "Hello world - Meu nome é Kauan";
+    $idade = 18;
+    $nome = "Kauan";
+    $altura = 1.69;
+    $solteiro = true;
+    /*$solteiro = 1;
+    echo $solteiro;
+    echo 10 < 12;
+    CONST IDADE = 10;
+    echo IDADE;
+    if(10 > 12)
+    {
+        echo "falso";
+    }else
+    {
+        echo "verdadeiro ";
+    }
+
+    $nota1 = 6;
+    if($nota1 >= 6){
+        echo "Aprovado";
+    }else if($nota1 < 6 && $nota1 >= 2){
+        echo "Ainda tem chance";
+    }*/
+
+    /*$nota1 = 8;
+    $nota2 = 9;
+    $media = $nota1+$nota2;
+    $media /= 2;
+    if($media >= 6){
+        echo "Aprovado";
+    }else{
+        echo "Reprovado";
+    }*/
+
+    //echo "<br> <br>" . $nome . "<br>";
+
+    for($i = 1; $i < 545199; $i++){
+        if($i%2==0){
+            echo "Número " . $i . "<br>";}}
+
+?>
